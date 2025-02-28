06c9d3eddb1e7c3e5249a6d0bcd8b47d6d3d5c5c
rexvel <rexvelele@gmail.com>
2025-02-28 20:51:21 +0200

    chore: update mocks

--- src/test/mocks.ts
+++ src/test/mocks.ts
@@ -7,13 +7,9 @@ export const mockFetch = vi.fn() as MockFetch;
 
 export const mockBtoa = vi.fn(_ => 'mocked-base64-string');
 
-export const createSuccessResponse = (data: ArrayBuffer): MockResponse => ({
-  ok: true,
-  arrayBuffer: vi.fn().mockResolvedValue(data),
+const baseResponseProps: MockResponse = {
   headers: new Headers(),
   redirected: false,
-  status: 200,
-  statusText: 'OK',
   type: 'basic',
   url: '',
   clone: vi.fn(),
@@ -23,24 +19,22 @@ export const createSuccessResponse = (data: ArrayBuffer): MockResponse => ({
   text: vi.fn(),
   blob: vi.fn(),
   formData: vi.fn(),
+};
+
+export const createSuccessResponse = (data: ArrayBuffer): MockResponse => ({
+  ...baseResponseProps,
+  ok: true,
+  arrayBuffer: vi.fn().mockResolvedValue(data),
+  status: 200,
+  statusText: 'OK',
 });
 
 export const createErrorResponse = (status = 500): MockResponse => ({
+  ...baseResponseProps,
   ok: false,
   status,
-  headers: new Headers(),
-  redirected: false,
   statusText: 'Internal Server Error',
-  type: 'basic',
-  url: '',
-  clone: vi.fn(),
-  body: null,
-  bodyUsed: false,
   arrayBuffer: vi.fn(),
-  json: vi.fn(),
-  text: vi.fn(),
-  blob: vi.fn(),
-  formData: vi.fn(),
 });
 
 export const setupMocks = () => {
