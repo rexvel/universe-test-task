e3c089a0fa69a9b7a75d6c72d3599491334cbb97
rexvel <rexvelele@gmail.com>
2025-02-28 13:44:41 +0200

    fix: configure eslint & pre-commit-hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import airbnb from 'eslint-config-airbnb-typescript'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import airbnb from 'eslint-config-airbnb-typescript';
+import prettier from 'eslint-plugin-prettier';
+import eslintPluginPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended, 
-      ...tseslint.configs.recommended,
-      ...airbnb.configs.recommended,
-    ],
+    extends: [js.configs.recommended, eslintPluginPrettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,13 +24,13 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettier,
+      '@typescript-eslint': tseslint.plugin,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'import/prefer-default-export': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/jsx-props-no-spreading': 'off',
@@ -45,6 +44,7 @@ export default tseslint.config(
           tsx: 'never',
         },
       ],
+      'prettier/prettier': 'error',
     },
     settings: {
       'import/resolver': {
@@ -52,4 +52,4 @@ export default tseslint.config(
       },
     },
   },
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,38 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+/// <reference types="vitest" />
+
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, normalizePath } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const cMapsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'),
+);
+const standardFontsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'standard_fonts'),
+);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: cMapsDir, dest: '' },
+        { src: standardFontsDir, dest: '' },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': '/src',
     },
   },
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/vitest.setup.ts',
+    include: ['**/?(*.)test.ts?(x)'],
+  },
+});
