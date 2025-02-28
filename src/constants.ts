d64ee252942977b6f2c26b3f86a406f6b471f7c3
rexvel <rexvelele@gmail.com>
2025-02-28 21:30:33 +0200

    fix: use https protocol

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const apiURl = 'http://95.217.134.12:4010';
+export const apiURl = 'https://95.217.134.12:4010';
 export const apiKey = import.meta.env.VITE_API_KEY;
 export const DOCS_NAME_MAX_DISPLAYED_CHARS = 30;
 export const SAVED_DOCS_EMPTY_LENGTH = 0;
