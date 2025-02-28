87313bed77694a316217715a2267d6e3db3b4116
rexvel <rexvelele@gmail.com>
2025-02-28 13:56:12 +0200

    fix: build errors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,35 +1,34 @@
 import { apiKey, apiURl } from '@/constants';
 
 export const handleErrorMessage = (error: unknown): string => {
-    if (error instanceof Error) {
-        return error.message;
-    }
-
+  if (error instanceof Error) {
+    return error.message;
+  }
+  return 'Unknown error';
 };
 
-export const convertToPdf = async (text: string, options: { timeout?: number } = {}): Promise<string | null> => {
-    const apiUrl = `${apiURl}/create-pdf?apiKey=${apiKey}`;
+export const convertToPdf = async (text: string): Promise<string | null> => {
+  const apiUrl = `${apiURl}/create-pdf?apiKey=${apiKey}`;
 
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/pdf',
+      },
+      body: JSON.stringify({ text }),
+    });
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/pdf',
-            },
-            body: JSON.stringify({ text }),
-        });
-
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const buffer = await response.arrayBuffer();
-        const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer)));
-        return base64String;
-    } catch (error) {
-
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const buffer = await response.arrayBuffer();
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    return base64String;
+  } catch (error) {
+    console.error('Error converting to PDF:', error);
+    return null;
+  }
 };
